255940c4719126bdb26c5f99564346fc7562f16c
nathansteuer <47091212+nathansteuer@users.noreply.github.com>
2020-05-15 14:30:34 +0100

    Update URL_Classifier2.py

--- URL_Classifier2.py
+++ URL_Classifier2.py
@@ -97,7 +97,7 @@ def prefsuf(tldextract_output):
 def subdomain(tldextract_output):
     subdomain=tldextract_output.subdomain
     if subdomain=="" or subdomain=="www":
-        return 0
+        return 1
     dot=1
     if "www" in subdomain:
         subdomain = subdomain.replace("www.","");
