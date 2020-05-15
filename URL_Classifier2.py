135ae898a9865443800c5e72ee8a41559e3bc860
nathansteuer <47091212+nathansteuer@users.noreply.github.com>
2020-05-15 17:39:36 +0100

    Update URL_Classifier2.py

--- URL_Classifier2.py
+++ URL_Classifier2.py
@@ -255,7 +255,7 @@ def get_test_features(urls):
         tldextract_output = tldextract.extract(url)
         site = tldextract_output.subdomain + '.' + tldextract_output.domain + '.' + tldextract_output.suffix
         #Avoid duplication of work for repeat urls
-        if site in already_visited:
+        if site in already_analysed:
             features[i] = already_analysed[site]
             continue
             
